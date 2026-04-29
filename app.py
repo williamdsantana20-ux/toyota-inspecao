74f8608d1df57ef75a4513483bf3073afb27ebf6
williamdsantana20-ux <williamdsantana20@gmail.com>
2026-04-29 16:44:28 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+# carregar modelo
+model = load_model("modelo_ok_nok.h5")
+
+st.title("Inspeção Visual Toyota 150D")
+
+uploaded_file = st.file_uploader("Escolha a imagem da peça", type=["jpg","png","jpeg"])
+
+if uploaded_file is not None:
+    img = image.load_img(uploaded_file, target_size=(224,224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0
+
+    pred = model.predict(img_array)
+
+    if pred[0][0] > 0.5:
+        st.success("✅ Peça OK")
+    else:
+        st.error("❌ Peça NOK")
